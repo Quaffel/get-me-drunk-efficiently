29e7241c527635272d19d36a8b429154ee56e6e9
Jonas Wilms <unknown@example.org>
2021-03-15 12:01:17 +0100

    Add mocked logic to App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,82 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { DrinkList } from './drinks/DrinkList';
+import { IDrinkAmount, IDrink } from "../../types";
+import { Banner } from './basic/Banner';
+import { Spinner } from './basic/Spinner';
+import SearchForm from './searchform/searchform';
+
+const drinkAmounts = [
+  { 
+    drink: { 
+      name: "Bloody Mary",
+      description: "An english original",
+      ingredients: [{ name: "Tomato"}],
+      instructions: []
+    }, 
+    amount: 2 
+  },
+  { 
+    drink: { 
+      name: "Kolakorn",
+      description: "An english original",
+      ingredients: [{ name: "Coca-Cola" }, { name: "Korn" }, { name: "Eis"}],
+      instructions: ["Korn und Kola mischen", "Eiswürfel hinzugeben"]
+    }, 
+    amount: 3 
+  }
+];
 
 function App() {
+  const [state, setState] = React.useState<{ pending?: true, loading?: true, result?: IDrinkAmount[], error?: string  }>({ pending: true });
+
+  async function startRecomendation(query: { ingredients: string[], weight: number, }) {
+    setState({ loading: true });
+
+    // TODO: Call backend here
+    await new Promise(res => setTimeout(res, 1_000));
+
+    setState({ result: drinkAmounts });
+  }
+
+
+  if(state.loading) {
+    return (
+      <Container>
+        <Spinner />
+      </Container>
+    );
+  }
+
+  if(state.error) {
+    return (
+      <Container>
+        <Banner.Warning title="An error occurred" text={state.error} />
+      </Container>
+    )
+  }
+
+  if(state.pending) {
+    return (
+      <Container>
+        <SearchForm submit={(weight, ingredients) => startRecomendation({ ingredients, weight })} />
+      </Container>
+    )
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <DrinkList drinkAmounts={drinkAmounts} />
+    </Container>
   );
+}
+
+function Container({ children }: React.PropsWithChildren<{}>) {
+  return <div className="app">
+    <div className="app-title">Get me drunk</div>
+    {children}
+  </div>
 }
 
 export default App;
